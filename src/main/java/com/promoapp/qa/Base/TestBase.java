0e763e4c5223cd8ab3fdbe6345fb6415ff5a265d
sunitagautam <sgautam@codeblock.co.ingit config --global user.name sunitagautam>
2020-04-10 22:53:09 +0530

    logintestcases

--- src/main/java/com/promoapp/qa/Base/TestBase.java
+++ src/main/java/com/promoapp/qa/Base/TestBase.java
@@ -17,13 +17,13 @@ import com.promoapp.qa.utils.TestUtil;
 public class TestBase {
 	public static WebDriver driver;
 	public static Properties prop;
-	public  static EventFiringWebDriver e_driver;
+	//public  static EventFiringWebDriver e_driver;
     
 	
 	public TestBase() { //constructor
 		try {
 			prop= new Properties();
-			FileInputStream ip= new FileInputStream("D:\\sunita\\PromoappAutomationS\\src\\main\\java\\com\\promoapp\\qa\\config\\config.properties");
+			FileInputStream ip= new FileInputStream("D:\\sunita\\promoappselenium\\src\\main\\java\\com\\promoapp\\qa\\config\\config.properties");
 		prop.load(ip);
 		}catch(FileNotFoundException e) {
 			e.printStackTrace();
@@ -38,7 +38,8 @@ public static void initialization(){
 	String browserName =prop.getProperty("browser");
 	if(browserName.equals("chrome")) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sonu\\Downloads\\chromedriver.exe");
-		WebDriver driver =new ChromeDriver(); 
+		driver =new ChromeDriver(); 
+		driver.get(prop.getProperty("url"));
 	}
 //	else if(browserName.equals("ff")) {
 //		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Sonu\\Downloads\\geckodriver.exe");
@@ -52,7 +53,7 @@ public static void initialization(){
 	driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT,TimeUnit.SECONDS);
 	driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT,TimeUnit.SECONDS);
 	
-	driver.get(prop.getProperty("url"));
+	//driver.get(prop.getProperty("url"));
 	
 }
 
